c6a29d00e9f13e60faec36415d5ea761fee1dd17
SPrayag <Shalini.Prayag@academytrainee.com>
2019-06-28 11:44:30 +0100

    Homepage stuff

--- home.js
+++ home.js
@@ -1,3 +1,44 @@
+const ownersDiv = document.getElementById('owners');
+function displayOwners() {
 
-makeRequest('GET', 'http://localhost:9966/petclinic/api/pets')
-    .then(value => console.log(value));
+    makeRequest('GET', 'http://localhost:9966/petclinic/api/owners')
+        .then(value => displayOwners1(value));
+
+
+}
+
+function displayOwners1(value) {
+
+    for (let owner of value) {
+        const oDiv = document.createElement('div');
+        oDiv.setAttribute('firstName', 'lastName');
+        oDiv.className = 'card m-4 p-2';
+        oDiv.addEventListener('click', () => handleOwnerClicked(owner));
+
+
+        let fName = document.createElement('h4');
+        fName.innerText = owner.firstName;
+
+        let lName = document.createElement('h4');
+        lName.innerText = owner.lastName;
+
+        let tel = document.createElement('h6');
+        tel.innerText = owner.telephone;
+
+        oDiv.append(fName, lName, tel);
+              
+        ownersDiv.append(oDiv);
+
+    }
+}
+
+function handleOwnerClicked(owner) {
+    console.log('Owner CLICKED!', owner);
+    sessionStorage.setItem('ownerID', owner.id);
+
+
+    window.location = 'owner.html';
+    return false;
+}
+
+displayOwners();
